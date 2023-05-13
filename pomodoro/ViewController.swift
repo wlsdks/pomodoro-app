74b4330f37ffc2d7c1a24ac549714dbbd16aba6a
jinan <ja.choi@sweetk.co.kr>
2023-05-14 01:48:13 +0900

    1. enum타입을 선언하고 그 안에 시작, 일시정지, 취소 3가지 경우를 넣어줌 2. enum타입을 통해 취소/실행 버튼을 눌렀을때 각각 동작하는 action함수의 내용을 코드로 작성해줌 여기서 메서드를 몇개 만들어서 사용함

--- pomodoro/ViewController.swift
+++ pomodoro/ViewController.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+// MARK: - emum타입 선언
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
+
 class ViewController: UIViewController {
 
     // MARK: - outlet변수 선언
@@ -18,19 +25,65 @@ class ViewController: UIViewController {
     
     //timer에 저장된 시간을 초로 저장하는 변수
     var duration = 60
+    // timer의 상태를 가진 변수 선언 - 초기값은 end
+    var timerStatus: TimerStatus = .end
     
+    // MARK: - 앱 실행시 가장 먼저 동작
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func tapCancelButton(_ sender: UIButton) {
-        
+        configureToggleButton()
     }
     
+    // MARK: - timer버튼 설정
+    func setTimerInfoViewVisible(isHidden: Bool) {
+        self.timerLabel.isHidden = isHidden
+        self.progressView.isHidden = isHidden
+    }
+    
+    // MARK: - 토글버튼 설정
+    func configureToggleButton() {
+        // 버튼이 normal이면 시작으로바뀌고 selected면 일시정지로 바뀜
+        self.toggleButton.setTitle("시작", for: .normal)
+        self.toggleButton.setTitle("일시정지", for: .selected)
+    }
+
+    // MARK: - 취소버튼 액션
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+        switch self.timerStatus {
+        case .start, . pause:
+            self.timerStatus = .end
+            self.cancelButton.isEnabled = false
+            self.setTimerInfoViewVisible(isHidden: true)
+            self.datePicker.isHidden = false
+            self.toggleButton.isSelected = false
+            
+        default:
+            break
+        }
+    }
+    
+    // MARK: - 시작버튼 액션
     @IBAction func tapToggleButton(_ sender: UIButton) {
         // 설정한 시간을 duration에 넣어준다.
         self.duration = Int(self.datePicker.countDownDuration)
+        
+        // 버튼 누를때마다 상태를 변경시켜준다.
+        switch self.timerStatus {
+        // 맨 처음 시작버튼 누르면 end니까 바로아래 case가 동작
+        case .end:
+            self.timerStatus = .start
+            self.setTimerInfoViewVisible(isHidden: false)
+            self.datePicker.isHidden = true
+            self.toggleButton.isSelected = true
+            self.cancelButton.isEnabled = true
+        case .start:
+            self.timerStatus = .pause
+            self.toggleButton.isSelected = false
+        case .pause:
+            self.timerStatus = .start
+            self.toggleButton.isSelected = true
+        }
+        
         debugPrint(self.duration)
     }
     
