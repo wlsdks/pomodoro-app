fd508096dc4b18a2991430f361b352f1029b4d32
jinan <ja.choi@sweetk.co.kr>
2023-05-14 02:12:56 +0900

    1. animation효과를 넣어줘서 타이머 시작과 취소시 서로 화면전환이 부드럽게 되도록 한다.

--- pomodoro/ViewController.swift
+++ pomodoro/ViewController.swift
@@ -88,8 +88,11 @@ class ViewController: UIViewController {
         }
         self.timerStatus = .end
         self.cancelButton.isEnabled = false
-        self.setTimerInfoViewVisible(isHidden: true)
-        self.datePicker.isHidden = false
+        UIView.animate(withDuration: 0.5) {
+            self.timerLabel.alpha = 0
+            self.progressView.alpha = 0
+            self.datePicker.alpha = 1
+        }
         self.toggleButton.isSelected = false
         self.timer?.cancel()
         self.timer = nil // nil로 초기화해줘야함
@@ -117,8 +120,11 @@ class ViewController: UIViewController {
         case .end:
             self.currentSeconds = self.duration
             self.timerStatus = .start
-            self.setTimerInfoViewVisible(isHidden: false)
-            self.datePicker.isHidden = true
+            UIView.animate(withDuration: 0.5) {
+                self.timerLabel.alpha = 1
+                self.progressView.alpha = 1
+                self.datePicker.alpha = 0
+            }
             self.toggleButton.isSelected = true
             self.cancelButton.isEnabled = true
             self.startTimer()
