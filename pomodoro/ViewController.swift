1002416b604d0ee7a1eea05cf0b52a490804550c
jinan <ja.choi@sweetk.co.kr>
2023-05-14 01:02:48 +0900

    Initial Commit

---
+++ pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pomodoro
+//
+//  Created by 최진안 on 2023/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
